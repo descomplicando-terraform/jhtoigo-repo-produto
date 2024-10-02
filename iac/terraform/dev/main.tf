1b31225a88c2d17a42cf5fb86dd5820956e62282
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-10-02 14:48:30 -0300

    feat: add outputs

--- iac/terraform/dev/main.tf
+++ iac/terraform/dev/main.tf
@@ -1,18 +1,8 @@
-module "ecs_service" {
-  source       = "git::https://github.com/jhtoigo/terraform-aws-service.git?ref=v1.1.0"
-  name         = "produto"
+locals {
+  app_name     = "produto"
   project_name = "linuxtips-tf-final"
   environment  = "dev"
-  resource_tags = {
-    Environment = "dev"
-  }
-  ecs_cluster_name = data.aws_ssm_parameter.ecs_cluster_name.value
-  postgres_egress  = true
-  region           = "us-east-1"
-  vpc_id           = data.aws_ssm_parameter.vpc_id.value
-  container_port   = 3000
-  container_cpu    = 1024
-  container_memory = 2048
+  region       = "us-east-1"
   container_environments = {
     PORT     = "3000"
     NODE_ENV = "dev"
@@ -20,4 +10,24 @@ module "ecs_service" {
   container_secrets = {
     DATABASE_URL = "example"
   }
+  tags = {
+    Environment = "dev"
+  }
+}
+
+module "ecs_service" {
+  source                 = "git::https://github.com/jhtoigo/terraform-aws-service.git?ref=v1.1.1"
+  name                   = local.app_name
+  project_name           = local.project_name
+  environment            = local.environment
+  resource_tags          = local.tags
+  ecs_cluster_name       = data.aws_ssm_parameter.ecs_cluster_name.value
+  postgres_egress        = true
+  region                 = local.region
+  vpc_id                 = data.aws_ssm_parameter.vpc_id.value
+  container_port         = 3000
+  container_cpu          = 1024
+  container_memory       = 2048
+  container_environments = local.container_environments
+  container_secrets      = local.container_secrets
 }
