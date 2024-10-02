b4588b217211d9bfb19b5be38a44e6e8f1b91fb3
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-10-02 13:27:33 -0300

    fix: tflint warnings

--- iac/terraform/dev/data.tf
+++ iac/terraform/dev/data.tf
@@ -3,32 +3,8 @@ data "aws_ssm_parameter" "ecs_cluster_name" {
   name = "/linuxtips-tf-final/ecs-cluster/cluster_1/name"
 }
 
-data "aws_ssm_parameter" "ecs_cluster_id" {
-  name = "/linuxtips-tf-final/ecs-cluster/cluster_1/id"
-}
-
-data "aws_ssm_parameter" "ecs_log_group" {
-  name = "/linuxtips-tf-final/ecs-log-group"
-}
-
 ## VPC
-
 data "aws_ssm_parameter" "vpc_id" {
   name = "/linuxtips-tf-final/vpc/vpc_id"
-}
-
-## Service Discovery
-
-data "aws_ssm_parameter" "namespace" {
-  name = "/linuxtips-tf-final/namespace"
-}
-
-## Load Balancer
-data "aws_ssm_parameter" "load_balancer_arn" {
-  name = "/linuxtips-tf-final/lb/arn"
-}
-
-data "aws_ssm_parameter" "load_balancer_listener_arn" {
-  name = "/linuxtips-tf-final/lb/default_listener"
 }
 
--- iac/terraform/dev/provider.tf
+++ iac/terraform/dev/provider.tf
@@ -1,8 +1,16 @@
 provider "aws" {
   region = "us-east-1"
+
 }
 
 terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
     bucket = "jhtoigo-terraform-linuxtips"
     key    = "tf-intensivo/projeto-final/apps/node-pg-test"
